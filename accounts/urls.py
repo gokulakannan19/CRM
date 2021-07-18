b89efe1af0ecf438543db8e6e8dcf7b7a57ff5e4
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-07-18 19:04:14 +0530

    login and logout functionality added

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('customer/<str:pk>/', views.customer, name="customer"),
     path('products/', views.products, name="products"),
 
+    path('register/', views.register_page, name="register"),
+    path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_user, name="logout"),
+
     path('create_order/<str:pk>/', views.create_order, name="create_order"),
     path('update_order/<str:pk>/', views.update_order, name="update_order"),
     path('delete_order/<str:pk>/', views.delete_order, name="delete_order"),
