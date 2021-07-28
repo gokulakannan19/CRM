976305cf600748a05d9c365de0d9d07ad3cb26cd
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-07-28 22:26:05 +0530

    email operation added

--- crm/settings.py
+++ crm/settings.py
@@ -135,3 +135,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SMTP CONFIGURATION
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "codewithgokul@gmail.com"
+EMAIL_HOST_PASSWORD = "Madhavan_1917"
